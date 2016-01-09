e84f03f39f41a80aa574ab0ed9926835f5bc4b49
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-01-09 21:45:57 +0100

    Display path in city codes.

--- src/navigateme/NavigateMe.java
+++ src/navigateme/NavigateMe.java
@@ -5,10 +5,14 @@
  */
 package navigateme;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -31,13 +35,35 @@ public class NavigateMe {
 
         // get all headers
         Map<String, List<String>> map = connection.getHeaderFields();
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            System.out.println("Key : " + entry.getKey() + " ,Value : " + entry.getValue());
+        for (Map.Entry<String, List<String>> item : map.entrySet()) {
+            System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
         }
 
         // get Location header
         String location = connection.getHeaderField("Location");
         System.out.println(location);
+
+        URL pathURL = new URL(location);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(pathURL.openStream()));
+
+        String inputLine;
+        String pathResponse = "";
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+            pathResponse = inputLine;
+        }
+        in.close();
+        
+        Gson mygson = new Gson();
+        
+        ArrayList jsonArray = mygson.fromJson(pathResponse, ArrayList.class);
+        System.out.println(jsonArray);
+        
+        for( Object code : jsonArray) {
+            System.out.println(code);
+        }
+
 
     }
 
