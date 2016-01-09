a8db24ee4af381e6a72742aebd4dc145f4cdfc9b
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-01-09 22:34:32 +0100

    Convert city codes to full city names and display path.

--- src/navigateme/NavigateMe.java
+++ src/navigateme/NavigateMe.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,51 +32,31 @@ public class NavigateMe {
     public static void main(String[] args) throws MalformedURLException, IOException {
         // TODO code application logic here
 
-        URL url = new URL("http://pi.zetis.pw/krynskip/web-pathfinder/routes?from=WAW&to=KRK");
-
-        URLConnection connection = url.openConnection();
-
-        // get all headers
-        Map<String, List<String>> map = connection.getHeaderFields();
-        /*
-         for (Map.Entry<String, List<String>> item : map.entrySet()) {
-         System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
-         }*/
-
-        // get Location header
-        String location = connection.getHeaderField("Location");
-        System.out.println(location);
-
-        URL pathURL = new URL(location);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(pathURL.openStream()));
-
-        String inputLine;
-        String pathResponse = "";
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
-            pathResponse = inputLine;
-        }
-        in.close();
-
-        Gson mygson = new Gson();
-
-        ArrayList jsonArray = mygson.fromJson(pathResponse, ArrayList.class);
-        System.out.println(jsonArray);
-
-        for (Object code : jsonArray) {
-            System.out.println(code.toString());
-        }
-
         NavigateMe navigateMe = new NavigateMe();
 
         navigateMe.getCities();
+        
+        ArrayList<String> path = navigateMe.getPath("Warszawa", "Kraków");
+        
+        System.out.println("Twoja trasa:");
+        
+        for(String item : path) {
+            //System.out.println(item);
+            
+            String fullName = (String) navigateMe.getCityMap().get(item);
+            
+            System.out.println("- " + fullName);
+        }
 
     }
 
     public void addToCityMap(String key, String value) {
 
         this.cities.put(key, value);
+    }
+    
+    public HashMap getCityMap() {
+        return this.cities;
     }
 
     public void getCities() throws IOException {
@@ -93,8 +72,7 @@ public class NavigateMe {
             input = inputLine;
         }
 
-        System.out.println(input);
-
+        //System.out.println(input);
         Gson gson = new Gson();
 
         //Type type = new TypeToken<Map<String, String>>() {
@@ -108,16 +86,59 @@ public class NavigateMe {
             //System.out.println(city);
 
             String pair = city.replace("{", "").replace("}", "").replace(",", "").replace("id=", "").replace("name=", "");
-            System.out.println(pair);
-            
+            //System.out.println(pair);
+
             String parts[] = pair.split(" ");
-            
-            System.out.println("Key: " + parts[0] + " Value: " + parts[1]);
-            
+
+            //System.out.println("Key: " + parts[0] + " Value: " + parts[1]);
             addToCityMap(parts[0], parts[1]);
 
         }
 
+    }
+
+    public ArrayList<String> getPath(String from, String to) throws IOException {
+
+        ArrayList path = new ArrayList();
+
+        URL url = new URL("http://pi.zetis.pw/krynskip/web-pathfinder/routes?from=WAW&to=KRK");
+
+        URLConnection connection = url.openConnection();
+
+        // get all headers
+        Map<String, List<String>> map = connection.getHeaderFields();
+        /*
+         for (Map.Entry<String, List<String>> item : map.entrySet()) {
+         System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
+         }*/
+
+        // get Location header
+        String location = connection.getHeaderField("Location");
+        //System.out.println(location);
+
+        URL pathURL = new URL(location);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(pathURL.openStream()));
+
+        String inputLine;
+        String pathResponse = "";
+        while ((inputLine = in.readLine()) != null) {
+            //System.out.println(inputLine);
+            pathResponse = inputLine;
+        }
+        in.close();
+
+        Gson mygson = new Gson();
+
+        ArrayList jsonArray = mygson.fromJson(pathResponse, ArrayList.class);
+        //System.out.println(jsonArray);
+
+        for (Object code : jsonArray) {
+            //System.out.println(code.toString());
+            path.add(code.toString());
+        }
+        
+        return path;
     }
 
 }
