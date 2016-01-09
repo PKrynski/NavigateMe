c5259c2e61d70f44a39cad80964efe91f3bd1a60
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-01-09 22:59:52 +0100

    Code cleanup.

--- src/navigateme/NavigateMe.java
+++ src/navigateme/NavigateMe.java
@@ -12,8 +12,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -28,7 +26,6 @@ public class NavigateMe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
 
         NavigateMe navigateMe = new NavigateMe();
 
@@ -37,7 +34,7 @@ public class NavigateMe {
         } catch (IOException ex) {
             System.err.println("Nie mogę pobrać listy miast.");
         }
-        
+
         //navigateMe.ListCities();
         
         ArrayList<String> path = null;
@@ -46,14 +43,14 @@ public class NavigateMe {
         } catch (IOException ex) {
             System.err.println("Nie mogę pobrać trasy.");
         }
-        
+
         System.out.println("Twoja trasa:");
-        
-        for(String item : path) {
+
+        for (String item : path) {
             //System.out.println(item);
-            
+
             String fullName = (String) navigateMe.getCityMap().get(item);
-            
+
             System.out.println("- " + fullName);
         }
 
@@ -63,7 +60,7 @@ public class NavigateMe {
 
         this.cities.put(key, value);
     }
-    
+
     public TreeMap getCityMap() {
         return this.cities;
     }
@@ -76,50 +73,41 @@ public class NavigateMe {
 
         String inputLine;
         String input = "";
+
         while ((inputLine = in.readLine()) != null) {
-            //System.out.println(inputLine);
             input = inputLine;
         }
 
-        //System.out.println(input);
         Gson gson = new Gson();
 
-        //Type type = new TypeToken<Map<String, String>>() {
-        //}.getType();
-        //Map<String, String> myMap = gson.fromJson(input, type);
         ArrayList jsonArray = gson.fromJson(input, ArrayList.class);
 
         for (Object item : jsonArray) {
 
             String city = item.toString();
-            //System.out.println(city);
-
             String pair = city.replace("{", "").replace("}", "").replace(",", "").replace("id=", "").replace("name=", "");
-            //System.out.println(pair);
 
             String parts[] = pair.split(" ");
-
-            //System.out.println("Key: " + parts[0] + " Value: " + parts[1]);
             addToCityMap(parts[0], parts[1]);
 
         }
 
     }
-    
+
     public void ListCities() {
-        
+
         System.out.println("Dostępna miasta:");
-        
-        for( String key : this.cities.keySet() ) {
+
+        for (String key : this.cities.keySet()) {
             System.out.println("- " + this.cities.get(key));
         }
-        
+
     }
 
     public ArrayList<String> getPath(String from, String to) throws IOException {
 
         ArrayList path = new ArrayList();
-        
+
         String preURL = "http://pi.zetis.pw/krynskip/web-pathfinder/routes?from=" + from + "&to=" + to;
 
         URL url = new URL(preURL);
@@ -127,15 +115,12 @@ public class NavigateMe {
         URLConnection connection = url.openConnection();
 
         // get all headers
-        Map<String, List<String>> map = connection.getHeaderFields();
+        //Map<String, List<String>> map = connection.getHeaderFields();
         /*
          for (Map.Entry<String, List<String>> item : map.entrySet()) {
          System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
          }*/
-
-        // get Location header
         String location = connection.getHeaderField("Location");
-        //System.out.println(location);
 
         URL pathURL = new URL(location);
 
@@ -144,7 +129,6 @@ public class NavigateMe {
         String inputLine;
         String pathResponse = "";
         while ((inputLine = in.readLine()) != null) {
-            //System.out.println(inputLine);
             pathResponse = inputLine;
         }
         in.close();
@@ -152,13 +136,11 @@ public class NavigateMe {
         Gson mygson = new Gson();
 
         ArrayList jsonArray = mygson.fromJson(pathResponse, ArrayList.class);
-        //System.out.println(jsonArray);
 
         for (Object code : jsonArray) {
-            //System.out.println(code.toString());
             path.add(code.toString());
         }
-        
+
         return path;
     }
 
