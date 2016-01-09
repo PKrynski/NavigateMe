d3ace6cf24b1a3e30bc628a3501b22a591827e23
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-01-09 22:21:14 +0100

    Get city list with codes from the web app.

--- src/navigateme/NavigateMe.java
+++ src/navigateme/NavigateMe.java
@@ -12,7 +12,9 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +23,8 @@ import java.util.Map;
  * @author Paweł
  */
 public class NavigateMe {
+
+    HashMap<String, String> cities = new HashMap<>();
 
     /**
      * @param args the command line arguments
@@ -35,9 +39,10 @@ public class NavigateMe {
 
         // get all headers
         Map<String, List<String>> map = connection.getHeaderFields();
-        for (Map.Entry<String, List<String>> item : map.entrySet()) {
-            System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
-        }
+        /*
+         for (Map.Entry<String, List<String>> item : map.entrySet()) {
+         System.out.println("Key : " + item.getKey() + " ,Value : " + item.getValue());
+         }*/
 
         // get Location header
         String location = connection.getHeaderField("Location");
@@ -54,16 +59,64 @@ public class NavigateMe {
             pathResponse = inputLine;
         }
         in.close();
-        
+
         Gson mygson = new Gson();
-        
+
         ArrayList jsonArray = mygson.fromJson(pathResponse, ArrayList.class);
         System.out.println(jsonArray);
-        
-        for( Object code : jsonArray) {
-            System.out.println(code);
+
+        for (Object code : jsonArray) {
+            System.out.println(code.toString());
         }
 
+        NavigateMe navigateMe = new NavigateMe();
+
+        navigateMe.getCities();
+
+    }
+
+    public void addToCityMap(String key, String value) {
+
+        this.cities.put(key, value);
+    }
+
+    public void getCities() throws IOException {
+
+        URL cityListURL = new URL("http://pi.zetis.pw/krynskip/web-pathfinder/cities");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(cityListURL.openStream()));
+
+        String inputLine;
+        String input = "";
+        while ((inputLine = in.readLine()) != null) {
+            //System.out.println(inputLine);
+            input = inputLine;
+        }
+
+        System.out.println(input);
+
+        Gson gson = new Gson();
+
+        //Type type = new TypeToken<Map<String, String>>() {
+        //}.getType();
+        //Map<String, String> myMap = gson.fromJson(input, type);
+        ArrayList jsonArray = gson.fromJson(input, ArrayList.class);
+
+        for (Object item : jsonArray) {
+
+            String city = item.toString();
+            //System.out.println(city);
+
+            String pair = city.replace("{", "").replace("}", "").replace(",", "").replace("id=", "").replace("name=", "");
+            System.out.println(pair);
+            
+            String parts[] = pair.split(" ");
+            
+            System.out.println("Key: " + parts[0] + " Value: " + parts[1]);
+            
+            addToCityMap(parts[0], parts[1]);
+
+        }
 
     }
 
